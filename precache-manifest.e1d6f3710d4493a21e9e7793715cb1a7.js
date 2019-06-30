54c2fcf945355d10318176e9774e93bc3981dbfa
Elvin <elveng@gmail.com>
2019-06-30 16:07:34 -0400

    reset 3 previosu commits

---
+++ precache-manifest.e1d6f3710d4493a21e9e7793715cb1a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122ef386bc0ee377b5c0fcfd55756723",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "07153490846b0e4623e3",
+    "url": "/react-clicky-game/static/css/main.7bd63cba.chunk.css"
+  },
+  {
+    "revision": "7f929daced632cb9be69",
+    "url": "/react-clicky-game/static/js/2.5f344e52.chunk.js"
+  },
+  {
+    "revision": "07153490846b0e4623e3",
+    "url": "/react-clicky-game/static/js/main.7a3600c9.chunk.js"
+  },
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  }
+]);
